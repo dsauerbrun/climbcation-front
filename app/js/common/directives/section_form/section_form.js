0bc6a0c9cc28de89c20240d67bf23a49dd9a6da5
dsauerbrun <dsauerbrun11@ucsbalum.com>
2016-06-15 23:36:46 -0600

    dont set default distance data

--- app/js/common/directives/section_form/section_form.js
+++ app/js/common/directives/section_form/section_form.js
@@ -24,7 +24,7 @@ sectionForm.directive('sectionform', function($http){
 
 
 sectionForm.controller('SectionFormController', function(fastLevenshteinService,$sce, $scope,$q,$http,Upload,$location, helperService, $route, $timeout){
-	$scope.locationObj = {'submitter_email':'','name':'','country':'','continent':'','airport':'','price_floor':'','price_ceiling':'','months':{},'accommodations':{},'climbingTypes':{},'grade':'', 'sections':[], closestAccommodation: '<1 mile'};
+	$scope.locationObj = {'submitter_email':'','name':'','country':'','continent':'','airport':'','price_floor':'','price_ceiling':'','months':{},'accommodations':{},'climbingTypes':{},'grade':'', 'sections':[], closestAccommodation: ''};
 	var emptySection = {'previewOff':true, 'title':'','body':''}
 	$scope.existMessage= 'This location already exists. If you would like to edit it, please find it on the home page and edit it there';
 	$scope.helperService = helperService;
